5efbd1f167006e24f804aa6d3d91ee9885437b87
thd3rBoy <thd3rboy@github.io>
2022-07-05 06:23:58 +0700

    added options.py

---
+++ lib/core/options.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import argparse, sys
+
+class OptionsArgs:
+
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(
+            prog="TrEnum", 
+            usage=f"{sys.argv[0]} [ -m options mode [default arguments] ] [ -d domain [default arguments] ] [ arguments ]", 
+            epilog="See github.com/thd3rBoy/TrEnum for more info"
+        )
+        self.parser._optionals.title = "Options"
+        self.parser.add_argument(
+            "-v",
+            "--version",
+            action="version",
+            version="%(prog)s 1.0"
+        )
+        self.parser.add_argument(
+            "-m",
+            "--options-mode",
+            dest="mode",
+            action="store",
+            required=True,
+            help="Mode options can be set by choice (ex: subs, norx)"
+        )
+        self.parser.add_argument(
+            "-d",
+            "--domain",
+            required=True,
+            help="Domain name to enumerate"
+        )
+        self.parser.add_argument(
+            "-o",
+            "--output",
+            help="file to write output results"
+        )
+        self.parser.add_argument(
+            "-s",
+            "--silent",
+            action="store_true",
+            default=False,
+            help="Silent mode, Only print the result and ignore the error"
+        )
+        self.parser.add_argument(
+            "--no-subs",
+            action="store_false",
+            default=True,
+            help="Use this to exclude subdomains in searches. This only applies to the norx mode option"
+        )
+
+        return
+
+    def _options(self):
+        args = self.parser.parse_args()
+        return args
